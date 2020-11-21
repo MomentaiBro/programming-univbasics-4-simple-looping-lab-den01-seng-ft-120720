37ef9007d4685e166ba76372ed28e46ddae223ca
Alexander S <alx.sherwin@gmail.com>
2020-11-21 18:34:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,11 @@ def loop_message_n_times (message, number)
   number += 1 
   end
 end
+
+def output_array(array)
+  counter = 0 
+  while array[counter] do
+    puts arrary[counter]
+    counter += 1
+  end
+end
