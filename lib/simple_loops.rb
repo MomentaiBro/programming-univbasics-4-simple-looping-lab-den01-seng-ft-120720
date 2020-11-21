72a5ede9859aa3aa44847ded70a5d7834b813256
Alexander S <alx.sherwin@gmail.com>
2020-11-21 18:20:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,12 @@
-# Write your methods here
+def loop_message_five_times (message)
+  message = "Hello World."
+  5.times {puts message}
+
+end
+
+def loop_message_n_times (message, number)
+  number = 5
+  while number < 5 
+  puts message
+  number += 1
+end
