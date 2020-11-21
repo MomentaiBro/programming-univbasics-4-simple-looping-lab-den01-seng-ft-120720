01f1b440310696ecc0e014ade4978b5c272886e1
Alexander S <alx.sherwin@gmail.com>
2020-11-21 18:50:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,7 +28,7 @@ end
 def return_string_array (array)
   count = 0 
   new_array = []
-  while count < arrary.length do
+  while count < array.length do
     new_array.push(array[count].to_s)
     count += 1
   end
